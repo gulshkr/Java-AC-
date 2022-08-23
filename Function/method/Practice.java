2c3ac5367c5dc2ba71160699baad228e68ae7cd2
gulshkr <gulshansinghmuzz.591@gmail.com>
2022-08-23 18:36:50 +0530

    done

--- Function/method/Practice.java
+++ Function/method/Practice.java
@@ -2,9 +2,9 @@ package Function.method;
 import java.util.*;
 public class Practice {
     //1st Java method to compute the average of three numbers
-    public static int average(int a,int b,int c){
-        return (a+b+c)/3;
-    }
+    // public static int average(int a,int b,int c){
+    //     return (a+b+c)/3;
+    // }
 
     //2nd method named isEven acepts an int argument
     // public static boolean isEven(int n){
@@ -15,19 +15,29 @@ public class Practice {
     // }
 
     //3rd check number if it is palindrome
-    // public static boolean isPalindrome(int n){
-    //     int reverse = 0;
-    //     int myNum = n;
-    //     while(n>0){
-    //         int lastdigit = n%10;
-    //         reverse = (reverse*10)+lastdigit;
-    //         n = n/10;
-    //         if(myNum==reverse){
-    //             return true;
-    //         }
-    //     }
-    //     return false;
-    // }
+    public static boolean isPalindrome(int n){
+        int myNum = n;
+        int reverse = 0;
+
+        if(myNum==0){
+            return true;
+        }
+        if(myNum%10==0 || myNum<0){
+            return false;
+        }
+
+        while(myNum!=0){
+            int lastdigit = n%10;
+            reverse = reverse*10 + lastdigit;
+            myNum = myNum/10;
+        } 
+
+        if(myNum == reverse){
+            return false;
+        } else{
+        return true;
+        }
+      }
 
     //4th search about & use the following methods of the math class in java Math.min() Math.mix() Math.sqrt() Math.pow() Math.avg() Math.abs()
     //min and max
@@ -46,10 +56,13 @@ public class Practice {
     
     
     public static void main(String[] args) {
+        System.out.println("Enter ");
         // System.out.println("The average of three numbers are: "+average(2, 3, 10));
         // System.out.println("the number you enterd is : "+isEven(11));
-        // System.out.println(isPalindrome(121));
-        // System.out.println(isPalindrome(123));
+        // System.out.println(isPalindrome(0));
+        Scanner sc = new Scanner(System.in);
+        int myNum = sc.nextInt();
+        System.out.println(isPalindrome(myNum));
         // Practice obj = new Practice();
         // obj.maxMin(2,3);
         // obj.sQrt(4);
