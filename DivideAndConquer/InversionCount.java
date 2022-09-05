0947c61e195417355503798c53354cc2ade3cdd5
gulshkr <gulshansinghmuzz.591@gmail.com>
2022-09-05 21:34:57 +0530

    #javaisfun

--- DivideAndConquer/InversionCount.java
+++ DivideAndConquer/InversionCount.java
@@ -15,10 +15,63 @@ public class InversionCount {
     //     return invCount;
     // }
 
-    //2.
+    //2. using merge approach T.C - O(nlogn)
+    public static int merge(int arr[],int si,int mid,int ei){
+        int i = si,j = mid,k =0;
+        int invCount = 0;
+        int temp[] = new int[(ei-si+1)];
+
+        //when both condition are true then
+        while((i<mid) && (j<=ei)){
+            if(arr[i]<=arr[j]){
+                temp[k] = arr[i];
+                k++;
+                i++;
+            } else{
+                temp[k] = arr[j];
+                invCount+=(mid-i);
+                k++;
+                j++;
+            }
+        }
+
+        //if both condition are not true then this
+        while(i<mid){
+            temp[k]=arr[i];
+            k++;
+            i++;
+        }
+        while(j<=ei){
+            temp[k]= arr[j];
+            k++;
+            j++;
+        }
+        for(i=si,k=0;i<=ei;i++,k++){
+            arr[i] = temp[k];
+        }
+        return invCount;
+    }
+    private static int mergeSort(int arr[],int si,int ei){
+        int invCount = 0;
+        if(si<ei){
+            int mid = (si+ei)/2;
+
+            invCount += mergeSort(arr, si, mid);
+            invCount += mergeSort(arr, mid+1, ei);
+            invCount += merge(arr, si, mid+1, ei);
+        }
+        return invCount;
+    }
+
+    // public static int getInversionCount(int arr[]){
+    //     int n = arr.length;
+    //     return mergeSort(arr, 0, n-1);
+    // }
     public static void main(String[] args) {
         int arr[] ={1,20,6,4,5};
-        System.out.println("Inversion count = "  +getInversionCount(arr));
+        int n = arr.length;
+        // System.out.println("Inversion count = "  +getInversionCount(arr));
+        System.out.println(mergeSort(arr, 0, n-1));
     }
     
 }
--- DivideAndConquer/MajorityElement.java
+++ DivideAndConquer/MajorityElement.java
@@ -19,11 +19,57 @@ public class MajorityElement {
     //     return -1;
     // }
     
-    
+    //2nd - approach - using Divide and conquer - merge sort
+    public static int countInRange(int nums[],int num,int si,int ei){
+        int count = 0;
+        for(int i=si;i<=ei;i++){
+            if(si==ei){
+                count++;
+            }
+        }
+        return count;
+    }
+
+    public static int majorityElememtRec(int nums[],int si,int ei){
+        //Base case - the only element in an array of size 1 is the majority
+        if(si==ei){
+            return nums[si];
+        }
+
+        //Recurse the element  in two halves right and left of this slice
+        int mid = si+(ei-si)/2;
+
+        int left = majorityElememtRec(nums, si, mid);
+        int right = majorityElememtRec(nums, mid+1, ei);
+
+        //if the two halves agree on the majority element, return the element
+
+        if(left == right){
+            return left;
+        }
+
+        //Otherwise, count each element and return the "winner"
+
+        int leftCount = countInRange(nums, left,si,ei);
+        int rightCount = countInRange(nums, right,si,ei);
+
+        //Compare both leftCount and rightCount and return left as winner
+        return leftCount>rightCount ? left : right;
+    }
+
+    public static int majorityElement(int nums[]){
+        return majorityElememtRec(nums, 0, nums.length-1);
+    }
 
     public static void main(String[] args) {
-        int arr[] = {2,2,1,1,1,2,2};
-        System.out.println(countMajorityElement(arr));
+        //1st approach - brute force
+        // int arr[] = {2,2,1,1,1,2,2};
+        // System.out.println(countMajorityElement(arr));
+
+        //2nd approach - divide and conquer using merge sort
+        // int nums[] = {2,2,1,1,1,2,2};
+        int nums[] = {3,3,4};
+        System.out.println(majorityElement(nums));
     }
     
 }
