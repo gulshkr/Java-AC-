cec8b1e3e9c6628f51214d8ff7266c2d345862da
gulshkr <gulshansinghmuzz.591@gmail.com>
2022-09-29 15:57:36 +0530

    #Alpha #javaisfun

---
+++ BinaryTree/BuildTreePreorder.java
@@ -0,0 +1,42 @@
+package BinaryTree;
+import java.util.*;
+//Build tree preorder
+public class BuildTreePreorder {
+    //first create a class Node
+    static class Node{
+        int data;
+        Node left;
+        Node right;
+
+        //constructor  under which ddata will come
+        Node(int data){
+            this.data = data;
+            this.left = null;
+            this.right = null;
+        }
+    }
+
+    static class BinaryTree{
+        static int idx = -1;
+        //function buildTree
+        public static Node buildTree(int nodes[]){
+            idx++;
+            if(nodes[idx] == -1){
+                return null;
+            }
+            Node newNode = new Node(nodes[idx]);
+            newNode.left = buildTree(nodes);
+            newNode.right = buildTree(nodes);
+
+            return newNode;
+        }
+    }
+    public static void main(String[] args) {
+        int nodes[] = {1,2,4,-1,-1,5,-1,-1,3,-1,6,-1,-1};
+        BinaryTree tree = new BinaryTree();
+        Node root = tree.buildTree(nodes);
+        System.out.println(root.data);
+        
+    }
+    
+}
