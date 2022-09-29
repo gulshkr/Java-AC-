56dc3d380dd555d55c942e14ff6d69cb02a6ca1f
gulshkr <gulshansinghmuzz.591@gmail.com>
2022-09-29 22:59:13 +0530

    #Alpha #java

--- BinaryTree/BuildTreePreorder.java
+++ BinaryTree/BuildTreePreorder.java
@@ -30,12 +30,87 @@ public class BuildTreePreorder {
 
             return newNode;
         }
+
+        //preorder traversal
+        public static void preorder(Node root){
+            if(root == null){
+                return;
+            }
+            System.out.print(root.data+" ");
+            preorder(root.left);
+            preorder(root.right);
+        }
+
+        //Inorder traversal
+        public static void inorder(Node root){
+            if(root == null){
+                return;
+            }
+            inorder(root.left);
+            System.out.print(root.data+" ");
+            inorder(root.right);
+        }
+
+        //postorder traversal
+        public static void postorder(Node root){
+            if(root==null){
+                return;
+            }
+            postorder(root.left);
+            postorder(root.right);
+            System.out.print(root.data+" ");
+        }
+
+        //Level order Traversal
+        public static void levelOrder(Node root){
+            if(root==null){
+                return;
+
+            }
+
+            Queue<Node> q = new LinkedList<>();
+            q.add(root);
+            q.add(null);
+
+            while(!q.isEmpty()){
+                Node currNode = q.remove();
+                if(currNode==null){
+                    System.out.println();
+                    if(q.isEmpty()){
+                        break;
+                    } else{
+                        q.add(null);
+                    }
+                } else{
+                    System.out.print(currNode.data+" ");
+                    if(currNode.left!=null){
+                        q.add(currNode.left);
+                    }
+                    if(currNode.right!=null){
+                        q.add(currNode.right);
+                    }
+                }
+            }
+        }
     }
     public static void main(String[] args) {
         int nodes[] = {1,2,4,-1,-1,5,-1,-1,3,-1,6,-1,-1};
         BinaryTree tree = new BinaryTree();
         Node root = tree.buildTree(nodes);
         System.out.println(root.data);
+
+        //preorder
+        tree.preorder(root);
+        System.out.println();
+        //inorder
+        tree.inorder(root);
+        System.out.println();
+        //postorder
+        tree.postorder(root);
+        System.out.println();
+        //levelorder
+        tree.levelOrder(root);
+        System.out.println();
         
     }
     
