ad2bb5441d0937355b5035ac05987b515780d3f6
gulshkr <gulshansinghmuzz.591@gmail.com>
2022-10-01 13:31:55 +0530

    #javaisfun

---
+++ BinaryTree/LowestCommonAncestorA2.java
@@ -0,0 +1,38 @@
+package BinaryTree;
+import java.util.*;
+//Approach 2nd for the finding of lowest common ancestor
+public class LowestCommonAncestorA2 {
+    static class Node{
+        int data;
+        Node left;
+        Node right;
+
+        //constructor  under which ddata will come
+        Node(int data){
+            this.data = data;
+            this.left = null;
+            this.right = null;
+        }
+    }
+    public static void main(String[] args) {
+         /*
+                     1
+                   /  \
+                  2    3
+                / \   / \ 
+               4   5 6   7
+        */
+        Node root = new Node(1);
+        root.left = new Node(2);
+        root.right = new Node(3);
+        root.left.left = new Node(4);
+        root.left.right = new Node(5);
+        root.right.left = new Node(6);
+        root.right.right = new Node(7);
+
+        int n1 = 4, n2 = 5;
+        
+        
+    }
+    
+}
