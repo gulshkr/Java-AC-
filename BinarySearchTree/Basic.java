168350527886aeff51a525de1559c197d87647c8
gulshkr <gulshansinghmuzz.591@gmail.com>
2022-10-03 20:21:43 +0530

    #javaisfun

--- BinarySearchTree/Basic.java
+++ BinarySearchTree/Basic.java
@@ -53,9 +53,44 @@ public class Basic {
     }
 
     //Delete in binary search treee
+    public static Node delete(Node root,int val){
+        if(root.data < val){
+            root.right = delete(root.right, val);
+        }
+        else if(root.data > val){
+            root.left = delete(root.left, val);
+        } else{  
+            //case -1 - leaf node
+            if(root.left == null && root.right == null){
+                return null;
+            }
+
+            //case -2 - sigle child
+            if(root.left == null){
+                return root.right;
+            }
+            else if(root.right == null){
+                return root.left;
+            }
+
+            //case - 3 - both children
+            Node Is = findInOrderSuccesoor(root.right);
+            root.data = Is.data;
+            root.right = delete(root.right, Is.data);
+        }
+        return root;
+    }
+
+    public static Node findInOrderSuccesoor(Node root){
+        while(root.left != null){
+            root = root.left;
+        }
+        return root;
+    }
     public static void main(String[] args) {
         // int values[] = {5,1,3,4,2,7};
-        int values[] = {8,9,7,1,4,3,2,5,6,10};
+        // int values[] = {8,9,7,1,4,3,2,5,6,10};
+        int values[] = {8,5,3,1,4,6,10,11,14};
         Node root = null;
 
         for(int i = 0;i<values.length;i++){
@@ -65,11 +100,17 @@ public class Basic {
         innorder(root);
         System.out.println();
 
-        if(search(root, 11)){
-            System.out.println("Found");
-        } else{
-            System.out.println("Not found");
-        }
+        root = delete(root, 1);
+        System.out.println();
+
+        innorder(root);
+
+        //this is for searcch 
+        // if(search(root, 11)){
+        //     System.out.println("Found");
+        // } else{
+        //     System.out.println("Not found");
+        // }
         
     }
     
