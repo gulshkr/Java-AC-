f250cab4cd2f38007978d4820cdecdbb2309466d
gulshkr <gulshansinghmuzz.591@gmail.com>
2022-10-04 18:47:17 +0530

    #java

---
+++ BinarySearchTree/MirrorABST.java
@@ -0,0 +1,17 @@
+package BinarySearchTree;
+//Mirror a tree
+public class MirrorABST {
+    static class Node{
+        int data;
+        Node left;  //left and right are already null 
+        Node right;
+        Node(int data){
+            this.data = data;
+        }
+    }
+    
+    public static void main(String[] args) {
+        
+    }
+    
+}
