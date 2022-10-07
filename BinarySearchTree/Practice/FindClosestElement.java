593410c3822c2b3d431e2fe3bd66c55c2cb428c7
gulshkr <gulshansinghmuzz.591@gmail.com>
2022-10-07 13:15:35 +0530

    #java

---
+++ BinarySearchTree/Practice/FindClosestElement.java
@@ -0,0 +1,9 @@
+package BinarySearchTree.Practice;
+import java.util.*;
+//Practice question - 2 - Find the closest element In Binart Search tree
+public class FindClosestElement {
+    public static void main(String[] args) {
+        
+    }
+    
+}
