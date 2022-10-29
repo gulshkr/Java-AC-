0701681718f8b3a4154a36a816635aaeaa87786c
gulshkr <gulshansinghmuzz.591@gmail.com>
2022-10-29 18:41:51 +0530

    #java

---
+++ Hashing/Basic.java
@@ -0,0 +1,16 @@
+package Hashing;
+import java.util.*;
+public class Basic {
+    public static void main(String[] args) {
+        //Create
+        HashMap<String, Integer> hm = new HashMap<>();
+
+        //Insert
+        hm.put("India", 100);
+        hm.put("China", 150);
+        hm.put("US", 50);
+
+        System.out.println(hm);
+    }
+    
+}
