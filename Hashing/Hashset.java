499484f9ad060fe0e02d4801e663cd6d848b41c5
gulshkr <gulshansinghmuzz.591@gmail.com>
2022-11-03 20:06:05 +0530

    hashset operations

---
+++ Hashing/Hashset.java
@@ -0,0 +1,26 @@
+package Hashing;
+import java.util.*;
+public class Hashset {
+    public static void main(String[] args) {
+        HashSet<Integer> set = new HashSet<>();
+
+        set.add(1);
+        set.add(2);
+        set.add(4);
+        set.add(2);
+        set.add(1);
+
+        System.out.println(set);
+
+        set.clear();
+        System.out.println("size of set is = "+set.size());
+        set.remove(2);
+        if(set.contains(2)){
+            System.out.println("set contains 2");
+        }
+
+        System.out.println(set.isEmpty());
+        
+    }
+    
+}
