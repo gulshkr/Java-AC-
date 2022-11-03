dd04a0eeff57dac43d72181f9d34d8a656f9e1c8
gulshkr <gulshansinghmuzz.591@gmail.com>
2022-11-03 07:55:27 +0530

    Hashmap & Treemap

---
+++ Hashing/LinkedHashmap.java
@@ -0,0 +1,13 @@
+package Hashing;
+import java.util.*;
+public class LinkedHashmap {
+    public static void main(String[] args) {
+        LinkedHashMap<String,Integer> lhm = new LinkedHashMap<>();
+        lhm.put("India", 100);
+        lhm.put("China", 150);
+        lhm.put("US", 50);
+
+        System.out.println(lhm);
+    }
+    
+}
---
+++ Hashing/Treemap.java
@@ -0,0 +1,14 @@
+package Hashing;
+import java.util.*;
+public class Treemap {
+    public static void main(String[] args) {
+        TreeMap<String,Integer> tm = new TreeMap<>();
+        tm.put("India",100);
+        tm.put("China",150);
+        tm.put("US",50);
+        tm.put("Indonesia",6);
+
+        System.out.println(tm);
+    }
+    
+}
