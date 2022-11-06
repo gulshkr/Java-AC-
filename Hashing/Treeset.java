1c21b7c89f92f41d42a7a67c909c6ec74909c908
gulshkr <gulshansinghmuzz.591@gmail.com>
2022-11-06 11:42:59 +0530

    Tree Set is sorted

---
+++ Hashing/Treeset.java
@@ -0,0 +1,13 @@
+package Hashing;
+import java.util.*;
+public class Treeset {
+    public static void main(String[] args) {
+        TreeSet<String> ts = new TreeSet<>();
+        ts.add("Delhi");
+        ts.add("Mumbai");
+        ts.add("Noida");
+        ts.add("Gurgaon");
+        System.out.println(ts);
+    }
+    
+}
