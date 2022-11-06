7a70a9a22ced449a1a2a094f6fb78c7af0313d7d
gulshkr <gulshansinghmuzz.591@gmail.com>
2022-11-06 18:10:37 +0530

    Union and intersection

---
+++ Hashing/UnionandIntersection.java
@@ -0,0 +1,38 @@
+package Hashing;
+import java.util.*;
+public class UnionandIntersection {
+    public static void main(String[] args) {
+        int arr1[] = {7,3,9};
+        int arr2[] = {6,3,9,2,9,4};
+        HashSet<Integer> set = new HashSet<>();
+
+        //union
+        for(int i =0;i<arr1.length;i++){
+            set.add(arr1[i]);
+        }
+
+        for(int i =0;i<arr2.length;i++){
+            set.add(arr2[i]);
+        }
+        System.out.println("union size = "+set.size());
+        System.out.println("union = "+set);
+
+        //intersection
+        set.clear();
+        for(int i = 0;i<arr1.length;i++){
+            set.add(arr1[i]);
+        }
+
+        int count = 0;
+        for(int i =0;i<arr2.length;i++){
+            if(set.contains(arr2[i])){
+                count++;
+                set.remove(arr2[i]);
+                System.out.println("intersection = "+set);
+            }
+        }
+        
+        System.out.println("intersection size = "+count);
+    }
+    
+}
