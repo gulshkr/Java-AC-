085ef2cc951dea7ba1371dceb0923c6fa0962908
gulshkr <gulshansinghmuzz.591@gmail.com>
2022-11-09 19:24:07 +0530

    search in trie

--- Tries/Basic.java
+++ Tries/Basic.java
@@ -29,11 +29,28 @@ public class Basic {
         curr.eow = true;
     }
 
+    //SEarch function
+    public static boolean search(String key){
+        Node curr = root;
+        for(int level = 0;level<key.length();level++){
+            int idx = key.charAt(level) - 'a';
+            if(curr.children[idx] == null){
+                return false;
+            }
+            curr = curr.children[idx];
+        }
+
+         return curr.eow == true;
+    }
+
     public static void main(String[] args) {
         String words[] = {"the","a","there","their","any","thee"};
         for(int i =0;i<words.length;i++){
             insert(words[i]);
         }
+
+        System.out.println(search("thee"));
+        System.out.println(search("an"));
     }
     
 }
